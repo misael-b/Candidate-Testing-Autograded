f6b5841c70285c25e134952db8324c388c699841
Misael Barcenas <mbarcenas@Misaels-Air.attlocal.net>
2023-09-21 10:35:54 -0500

    <completed part 2 of graded assignment>

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,6 +2,7 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
+
 // TODO 1.1a: Define candidateName // 
 let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
@@ -11,9 +12,23 @@ let candidateAnswer = '';
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [];
+let correctAnswers = [];
+let candidateAnswers =[];
+
+let question2 = "True or false: 5 kilometer == 5000 meters? ";
+let question3 = "(5 + 3)/2 * 10 = ? ";
+let question4 = "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ";
+let question5 = 'What is the minimum crew size for the ISS? ';
+
+questions = [question, question2, question3, question4, question5];
+
+let correctAnswer2 = "true";
+let correctAnswer3 = "40";
+let correctAnswer4 = "Trajectory";
+let correctAnswer5 = "3";
+
+correctAnswers = [correctAnswer, correctAnswer2, correctAnswer3, correctAnswer4, correctAnswer5];
 
 
 function askForName() {
@@ -23,18 +38,19 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question('Who was the first American woman in space?');
+  for (let i = 0; i <= questions.length - 1; i++){
+    candidateAnswers.push(input.question(questions[i]))
+  }
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === correctAnswer) {
-    console.log('Congratulations you have entered the correct answer!!!')
-  } else {
-    console.log('Sorry, that is incorrect. TRY AGAIN!')
-}
+
+  for (let i = 0; i <= questions.length - 1; i++){
+    console.log(`For question ${i+1} you answered '${candidateAnswers[i]}' and the correct answer is '${correctAnswers[i]}'.`)
+  }
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.

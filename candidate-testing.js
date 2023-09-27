b6aca510c5fad2dbbab5939285b15a209c7ccdd4
Misael Barcenas <mbarcenas@Misaels-Air.attlocal.net>
2023-09-27 18:12:28 -0500

    completed assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -33,13 +33,13 @@ correctAnswers = [correctAnswer, correctAnswer2, correctAnswer3, correctAnswer4,
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question('What is your name?');
+  candidateName = input.question('What is your name? ');
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   for (let i = 0; i <= questions.length - 1; i++){
-    candidateAnswers.push(input.question(questions[i]))
+    candidateAnswers.push(input.question(questions[i]));
   }
 
 }
@@ -47,14 +47,34 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  console.log(`Candidate Name: ${candidateName}`);
   for (let i = 0; i <= questions.length - 1; i++){
-    console.log(`For question ${i+1} you answered '${candidateAnswers[i]}' and the correct answer is '${correctAnswers[i]}'.`)
+    if ((candidateAnswers[i].toLowerCase().trim()) === (correctAnswers[i].toLowerCase().trim())) {
+      console.log(`Question ${i + 1}: ${questions[i]} 
+      Correct!! You answered '${candidateAnswers[i]}' and the correct answer is '${correctAnswers[i]}'.\n`);
+    } else {
+      console.log(`Question ${i + 1}: ${questions[i]} 
+      WRONG! You answered '${candidateAnswers[i]}' and the correct answer is '${correctAnswers[i]}'.\n `);
+    }
+    
   }
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
+  let numOfCorrectAnswers = 0;
+  for (let i = 0; i <= questions.length - 1; i++){
+    if ((candidateAnswers[i].toLowerCase().trim()) === (correctAnswers[i].toLowerCase().trim())) {
+      numOfCorrectAnswers += 1;
+    }
+    
+  }
+  grade = (numOfCorrectAnswers / questions.length) * 100;
+  
+  if (grade >= 80) {
+    console.log(`Congratulations! You have passed the test with a score of ${grade}%!!! \n Status: PASS`);
+  } else {
+    console.log(`Sorry you have failed this quiz with a score of ${grade}%!!!\n Status: FAILED`);
+  }
 
   return grade;
 }
